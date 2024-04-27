74e2046477c5753cdbde6acb2bfb546393d66a18
HP <HP@LAPTOP-CEU07CVV>
2024-04-27 15:49:59 +0530

    pattern7

--- javaPatternProgram/src/javaPatternProgram/pattern5.java
+++ javaPatternProgram/src/javaPatternProgram/pattern5.java
@@ -2,15 +2,11 @@ package javaPatternProgram;
 
 public class pattern5 {
 public static void main(String[] args) {
-	int count=0;
-		for(int j=1;j<=25;j++) {
-			System.out.print(j+" ");
-			count++;
-			if(count==5) {
-				System.out.println();
-				count=0;
-			}
+	for(int i=1;i<=5;i++) {
+		for(int j=5;j>=1;j--) {
+			System.out.print(j);
 		}
 		System.out.println();
 	}
+}
 }
---
+++ javaPatternProgram/src/javaPatternProgram/pattern6.java
@@ -0,0 +1,28 @@
+package javaPatternProgram;
+
+public class pattern6 {
+public static void main(String args[]) {
+	int count=0;
+		for(int j=1;j<=25;j++) {
+			System.out.print(j+" ");
+			count++;
+			if(count==5) {
+				System.out.println();
+				count=0;
+			}
+		}
+		System.out.println();
+		
+//     method 2nd 
+//	        int n = 5;
+//	      int k = 1;
+//	        for (int i = 1; i <= n; i++) {
+//	            for (int j = 1; j <= n; j++) {
+//	                System.out.printf("%2d ",k);
+//	                k++;
+//	            }
+//	            System.out.println();
+//	        }
+	    }
+	
+	}
