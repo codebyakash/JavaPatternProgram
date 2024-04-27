91498c7a14b8b441e7240e6fea73b73583c3046d
HP <HP@LAPTOP-CEU07CVV>
2024-04-27 19:14:01 +0530

    pattern10

---
+++ javaPatternProgram/src/javaPatternProgram/pattern10.java
@@ -0,0 +1,12 @@
+package javaPatternProgram;
+
+public class pattern10 {
+public static void main(String[] args) {
+	for(int i=1;i<=5;i++) {
+		for(int j=1;j<=3;j++) {
+			System.out.print(j+" "+i+" ");
+		}
+		System.out.println();
+	}
+}
+}
