0049f00c143c51545f079d052f32d815923989e8
HP <HP@LAPTOP-CEU07CVV>
2024-04-27 20:53:16 +0530

    pattern16

---
+++ javaPatternProgram/src/javaPatternProgram/pattern16.java
@@ -0,0 +1,12 @@
+package javaPatternProgram;
+
+public class pattern16 {
+public static void main(String[] args) {
+	for(int i=1;i<=5;i++) {
+		for(int j=i;j<=i+4;j++){
+		System.out.printf("%2d",j);
+		}
+		System.out.println();
+	}
+}
+}
