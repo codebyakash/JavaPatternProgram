2c9c4ebd4baa74e0ce8340b29eb43d70701c43cf
HP <HP@LAPTOP-CEU07CVV>
2024-04-27 16:05:26 +0530

    pattern8

---
+++ javaPatternProgram/src/javaPatternProgram/pattern8.java
@@ -0,0 +1,27 @@
+package javaPatternProgram;
+
+public class pattern8 {
+public static void main(String[] args) {
+	int n=5;
+	int k=2;
+	for(int i=1;i<=n;i++) {
+		for(int j=1;j<=n;j++) {
+			System.out.printf("%2d ",k);
+			k+=2;
+		}
+		System.out.println();
+	}
+//	2nd method
+	int count=0;
+	for(int i=1;i<=50;i++) {
+		if(i%2==0) {
+			System.out.printf("%2d ",i);
+			count++;
+		}
+		if(count==5) {
+			System.out.println();
+			count=0;
+		}
+	}
+}
+}
